879d427ba646f2f43ac7bbc132a4cc2d8b666689
marcocentonze <marco.centonze27@gmail.com>
2023-08-28 16:32:03 +0200

    add loop + if

--- mail/assets/main.js
+++ mail/assets/main.js
@@ -11,8 +11,22 @@ const userEmail = prompt("Inserisci la tua email");
 // creo array dove dichiaro tutte le email che sono sulla lista
  const emailList = ['luca.java@gmail.com','alessia.java@gmail.com','filippo.java@gmail.com','giovanni.java@gmail.com'];
 
+ // variabile per capire se l'utente è stato trovato nella lista
+let utenteTrovato = false; //nella condizione cambierà valore
+
 
 // controlla che sia nella lista di chi può accedere
+for (let i = 0; i < emailList.length; i++) {
+    const utente = emailList[i];
 
 
-// stampa un messaggio di controllo in base al risultato uscito
+    if (utente === userEmail) {
+        utenteTrovato = true;
+        console.log("Complimenti, fai parte della lista!"); // stampa in console risultato positivo
+    } else {
+       console.log('Mi dispiace,non fai parte della lista');// stampa in console risultato negativo
+    }
+
+  }
+
+
