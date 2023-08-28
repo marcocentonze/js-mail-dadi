76a86567e80e79d1515725f2414661c53c66a427
marcocentonze <marco.centonze27@gmail.com>
2023-08-28 16:57:19 +0200

    add bonus(to fix)

--- mail/assets/main.js
+++ mail/assets/main.js
@@ -7,6 +7,12 @@ Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in
 
 // chiedi all’utente la sua email
 const userEmail = prompt("Inserisci la tua email");
+// Seleziona l'elemento bottone
+const verificaBtn = document.getElementById("verifica");
+
+//creo l'evento cliccabile sul bottone
+verificaBtn.addEventListener("click", function() {
+    const userEmail = document.getElementById("email").value;//.value per ottenere il valore messo dall'utente
 
 // creo array dove dichiaro tutte le email che sono sulla lista
  const emailList = ['luca.java@gmail.com','alessia.java@gmail.com','filippo.java@gmail.com','giovanni.java@gmail.com'];
@@ -22,11 +28,13 @@ for (let i = 0; i < emailList.length; i++) {
 
     if (utente === userEmail) {
         utenteTrovato = true;
-        console.log("Complimenti, fai parte della lista!"); // stampa in console risultato positivo
+        document.getElementById("risultato").textContent = "Complimenti, fai parte della lista!";
+       // console.log("Complimenti, fai parte della lista!"); // stampa in console risultato positivo
     } else {
-       console.log('Mi dispiace,non fai parte della lista');// stampa in console risultato negativo
+        document.getElementById("risultato").textContent = "Mi dispiace, non fai parte della lista!";
+       // console.log('Mi dispiace,non fai parte della lista');// stampa in console risultato negativo
     }
 
   }
-
+});
 
