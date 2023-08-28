1731d7c3e5b6af27e03a73b4c8c30d0aa1e3c0fc
marcocentonze <marco.centonze27@gmail.com>
2023-08-28 16:09:48 +0200

    add pseudo codice

---
+++ mail/assets/main.js
@@ -0,0 +1,14 @@
+/*  -Mail
+Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Bonus:
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.*/
+
+
+
+// chiedi all’utente la sua email
+
+
+// controlla che sia nella lista di chi può accedere
+
+
+// stampa un messaggio di controllo in base al risultato uscito
